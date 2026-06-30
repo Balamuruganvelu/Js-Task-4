4e63f6153cea9ad957537a73ffc28978ad6af954
Balamuruganvelu <barathi5497@gmail.com>
2026-06-30 16:17:40 +0530

    Task11

---
+++ Task11.js
@@ -0,0 +1,7 @@
+let name = "Bala";
+let id = 2982;
+let department = "Development";
+
+console.log(`Welcome ${name}`);
+console.log(`Your Employee ID is EMP${id}`);
+console.log(`Department : ${department}`);
