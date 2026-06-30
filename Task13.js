9b2b037ba7ee4681e85dcf40143d1debef5a9fec
Balamuruganvelu <barathi5497@gmail.com>
2026-06-30 16:31:03 +0530

    Task14

---
+++ Task13.js
@@ -0,0 +1,9 @@
+let salary = 35000;
+let incrementPercent = 15;
+
+let incrementAmount = (salary * incrementPercent) / 100;
+let newSalary = salary + incrementAmount;
+
+console.log("Old Salary : " + salary);
+console.log("Increment Amount : " + incrementAmount);
+console.log("New Salary : " + newSalary);
