9fa4a13d0d0eb72568d3a148ebdeacd68275eeee
Balamuruganvelu <barathi5497@gmail.com>
2026-06-30 15:47:01 +0530

    Task04

---
+++ Task04.js
@@ -0,0 +1,21 @@
+let choice = 4;
+
+switch (choice) {
+    case 1:
+        console.log("You Ordered Pizza");
+        break;
+    case 2:
+        console.log("You Ordered Burger");
+        break;
+    case 3:
+        console.log("You Ordered Shawarma");
+        break;
+    case 4:
+        console.log("You Ordered Biryani");
+        break;
+    case 5:
+        console.log("You Ordered Juice");
+        break;
+    default:
+        console.log("Invalid Choice");
+}
