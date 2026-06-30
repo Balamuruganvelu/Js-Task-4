8739bd4fad5beaffbd9bf3cdf12de500753040e8
Balamuruganvelu <barathi5497@gmail.com>
2026-06-30 16:14:29 +0530

    Task10

---
+++ Task10.js
@@ -0,0 +1,10 @@
+let employee= {
+    name: "Balamurugan velu",
+    salary: 35000,
+    department: "Development",
+    experience: 1
+};
+
+console.log("Employee Name: " + employee.name);
+console.log("Department: " + employee.department);
+console.log("Experience: " + employee.experience);
